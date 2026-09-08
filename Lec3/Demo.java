c97607fbded10de0ea18e712e0416dcd7aa45227
hansrajathole <hansrajathole1@gmail.com>
2026-09-08 12:47:08 +0530

    variables and datatypes

---
+++ Lec3/Demo.java
@@ -0,0 +1,20 @@
+
+
+public class Demo {
+    public static void main(String [] args){
+       // System.out.println("Hello Chacha.....");
+
+        byte a = 127;
+        short b = 25376;
+        int x = 234567;
+        long l = 10000000000l;
+
+        float pi = 3.14f;
+        double db = 871325;
+        System.out.println(db);
+
+        char fav = 'A';
+        boolean isTrue = true;
+
+    }
+}
