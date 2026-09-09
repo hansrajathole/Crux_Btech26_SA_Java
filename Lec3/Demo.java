1f86680f86f5a0a0b08e20fb229fbe6f2493e4ad
hansrajathole <hansrajathole1@gmail.com>
2026-09-09 21:20:19 +0530

    basics

--- Lec3/Demo.java
+++ Lec3/Demo.java
@@ -5,11 +5,13 @@ public class Demo {
        // System.out.println("Hello Chacha.....");
 
         byte a = 127;
+        System.out.println("a ----> "+a);
         short b = 25376;
         int x = 234567;
         long l = 10000000000l;
 
-        float pi = 3.14f;
+        float pi = 3.14f; 
+        
         double db = 871325;
         System.out.println(db);
 
---
+++ Lec4/Demo2.java
@@ -0,0 +1,19 @@
+package Lec4;
+
+public class Demo2 {
+
+
+    public static void main(String [] args){
+
+        double age = 45.5;
+
+        System.out.println(age);
+        byte a = 127;
+        System.out.println("a ---->"+ a);
+        
+
+
+    }
+
+
+}
