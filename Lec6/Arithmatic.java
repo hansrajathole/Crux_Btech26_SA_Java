e7f1b2c9c4814989ea7c372de3d54365ae5107b2
hansrajathole <hansrajathole1@gmail.com>
2026-09-23 21:37:50 +0530

    Operators

---
+++ Lec6/Arithmatic.java
@@ -0,0 +1,17 @@
+package Lec6;
+
+public class Arithmatic {
+    public static void main(String[] args) {
+        int a = 20;
+        a = a+4; // ----> a = 20+4 --->24
+        System.out.println(a);
+
+        int b = 5;
+        a = a*b;
+        b = b+a;
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+}
---
+++ Lec6/Assignment.java
@@ -0,0 +1,14 @@
+package Lec6;
+
+public class Assignment {
+    public static void main(String[] args) {
+        int a = 20;
+        a +=4;
+        int b = 10;
+        b += a;
+        b /= 5;
+        b*=a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
